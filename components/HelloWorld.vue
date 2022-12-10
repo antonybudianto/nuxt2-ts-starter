fceb2f2020adbdd9532ea696958814233847246a
Antony <antony.budianto@tokopedia.com>
2022-12-10 19:11:16 +0700

    init commit

---
+++ components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+defineProps({
+  title: String,
+})
+
+const emit = defineEmits(['onClick'])
+
+const names = 'Antony'
+const textInput = ref('')
+
+function handleClickHello(value: string) {
+  emit('onClick', value)
+}
+</script>
+
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+    <div>
+      <div>Hello World!</div>
+      <button @click="() => handleClickHello(textInput)">click!</button>
+    </div>
+    <div>Name: {{ names }}</div>
+    <input
+      class="inputTxt"
+      type="text"
+      placeholder="Type here..."
+      v-model="textInput"
+    />
+    <div>input: {{ textInput }}</div>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.inputTxt {
+  margin: 15px 0;
+  padding: 10px 15px;
+  border: 2px solid lightslategray;
+  border-radius: 3px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <HelloWorld title="Welcome to Nuxt 2" @onClick="handleClick"
+    >HelloChild</HelloWorld
+  >
+</template>
+<script setup lang="ts">
+const handleClick = (value: string) => {
+  console.log('>> emit from HelloWorld: ', value)
+}
+</script>
